b6dbf00116cdcce6db9c8a90e007fab851075bbc
Yorn90104 <bingxiao526@gmail.com>
2025-03-02 17:38:06 +0800

    feat: P 加入靜態 Obj

--- P.ts
+++ P.ts
@@ -1,23 +1,30 @@
 type Mode = "Normal" | "SuperHHH" | "GreenWei" | "PiKaChu";
 
-class P {
+export const DefaultRates: Record<Mode, number> = {
+  Normal: 0,
+  SuperHHH: 0,
+  GreenWei: 0,
+  PiKaChu: 0,
+};
+
+
+export class P {
   static Obj: Record<string, P> = {};
 
   Code: string | null;
   Scores: number[];
-  Rates: Record<string, number>;
+  Rates: Record<Mode, number>;
   constructor(
     Code: string | null = null,
     Scores: number[] = [0, 0, 0],
-    Rates: Record<Mode, number> = {
-      Normal: 0,
-      SuperHHH: 0,
-      GreenWei: 0,
-      PiKaChu: 0,
-    }
+    Rates: Record<Mode, number> = DefaultRates
   ) {
     this.Code = Code;
     this.Scores = Scores;
     this.Rates = Rates;
+
+    if (this.Code && !P.Obj[this.Code]) {
+      P.Obj[this.Code] = this;
+    }
   }
 }
