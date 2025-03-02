b38dd9f9d68b8ef05c790ca282e6cbf19d25dbfe
Yorn90104 <bingxiao526@gmail.com>
2025-03-02 17:23:00 +0800

    feat: P & npm init

--- P.ts
+++ P.ts
@@ -1,8 +1,23 @@
+type Mode = "Normal" | "SuperHHH" | "GreenWei" | "PiKaChu";
+
 class P {
   static Obj: Record<string, P> = {};
 
-  Code: string|null;
-  constructor(Code: string|null = null) {
-    this.Code = Code ;
+  Code: string | null;
+  Scores: number[];
+  Rates: Record<string, number>;
+  constructor(
+    Code: string | null = null,
+    Scores: number[] = [0, 0, 0],
+    Rates: Record<Mode, number> = {
+      Normal: 0,
+      SuperHHH: 0,
+      GreenWei: 0,
+      PiKaChu: 0,
+    }
+  ) {
+    this.Code = Code;
+    this.Scores = Scores;
+    this.Rates = Rates;
   }
 }
