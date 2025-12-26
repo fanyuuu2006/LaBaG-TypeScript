a174ab97d0b4fd36b8e4a56ac0107f0f50a2b541
Yorn90104 <bingxiao526@gmail.com>
2025-12-26 11:20:56 +0800

    refactor: 調整匯入順序，新增 Mode 的匯出

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { LaBaG } from "./labag";
 import { modeList, ModeName } from "./modes";
 import { LaBaGEvent, Pattern, PatternName } from "./types";
+import { Mode } from "./mode";
 
 const labag = new LaBaG();
 modeList.forEach((mode) => {
-    labag.addMode(mode);
+  labag.addMode(mode);
 });
 export {
   labag,
+  LaBaG,
+  Mode,
   type LaBaGEvent,
   type Pattern,
   type PatternName,
