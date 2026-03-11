7b53669c9c93765d40cede69bfc6caf0664f8672
Yorn90104 <bingxiao526@gmail.com>
2026-03-11 13:50:42 +0800

    重構: 更新版本號至 2.6.6，調整 modes 的匯出方式以增強可讀性

--- src/modes/index.ts
+++ src/modes/index.ts
@@ -2,4 +2,4 @@ import greenwei from "./greenwei";
 import pikachu from "./pikachu";
 import superhhh from "./superhhh";
 
-export const modes = Object.values({ pikachu, greenwei, superhhh });
+export const modes = [pikachu, greenwei, superhhh] as const;
